87bb5d8df72c079d1f1d5b592e756732f4ad622b
Stefan Haslinger <stefan.haslinger@mittenin.at>
2012-10-13 04:48:42 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aio::Application.config.secret_token = 'd3a1093af59cdf2e92c799b8d77efcd8a773e9a9e5e8b2bd1b2351fd51d1f845f8709730c6b70fdf7ce8cf9d47885c0dd827a90f93fd454dfa6fcc8741770199'
